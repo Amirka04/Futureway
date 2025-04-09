597d2918cb91bba2e69c9128f479d993809729a2
Amirka04 <kokovamir6@gmail.com>
2025-04-09 15:34:57 +0300

    добавление README файлов

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-import flask
+from flask import Flask, request
 
-print("")
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return f"Hello World, bro!"
+
+
+@app.route('/hello')
+def hello():
+    return "Hello world again bro!"
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
